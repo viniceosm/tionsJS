b488defc3de8c67610b6deef281d84d6895f25ba
viniceosm <miiller.vini@gmail.com>
2018-08-02 09:54:56 -0300

    Busca repositorios usuario

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,45 @@
 var demo = new Vue({
 	el: '#app',
 	data: {
-
+		user: 'viniceosm',
+		branches: ['master', 'dev'],
+		currentBranch: 'master',
+		commits: null,
+		repos: null
 	},
-	methods: {
+	created: function () {
+		this.fetchData()
+	},
+	watch: {
+		currentBranch: 'fetchData'
+	},
+	computed: {
+		apiURL() {
+			return `https://api.github.com/users/${this.user}/repos`
+		}
+	},
+	filters: {
+		truncate: function (v) {
+			var newline = v.indexOf('\n')
+			return newline > 0 ? v.slice(0, newline) : v
+		},
+		formatDate: function (v) {
+			return v.replace(/T|Z/g, ' ')
+		}
+	},
 
+	methods: {
+		fetchData: function () {
+			var xhr = new XMLHttpRequest()
+			var self = this
+			xhr.open('GET', this.apiURL)
+			xhr.onload = function () {
+				self.repos = JSON.parse(xhr.responseText)
+			}
+			xhr.send()
+		}
 	}
-});
+})
 
 let titleProject = document.getElementById('titleProject');
 
