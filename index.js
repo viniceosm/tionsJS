a23d8cfc7aff2112b3b35e198d27bcdadd9709c0
Christopher Dopona Lopes <christopher@olostech.com>
2018-08-02 08:57:19 -0300

    Adiciona efeito no titulo do projeto.

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ var demo = new Vue({
 
 	}
 });
+
+let titleProject = document.getElementById('titleProject');
+
+window.setInterval(() => {
+	animacaoNomeProjeto();
+}, 5000);
+
+function animacaoNomeProjeto() {
+	$(titleProject).fadeOut(2000, function () {
+		titleProject.textContent = 'tionsJS'
+		$(titleProject).fadeIn(2000);
+	});
+};
